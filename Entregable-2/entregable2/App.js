954ce7580ba1c158bba9cd5d7577774cd49381cd
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-09-14 14:39:14 -0300

    [feature] - CSS properties added to list

--- Entregable-2/entregable2/App.js
+++ Entregable-2/entregable2/App.js
@@ -4,9 +4,13 @@ import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-nat
 
 export default function App() {
   const [textItem, setTextItem] = useState('');
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([{id: 1, value: 'Buy Water'}]);
+  console.log('useStates created');
 
-  const onHandlerChangeItem = (t) => { setTextItem(t)}
+  const [itemSelected, setItemSelected] = useState({});
+  const [modalVisible, setModalVisible] = useState([]);
+
+  const onHandlerChangeItem = (t) => { setTextItem(t) }
 
   const addTask = () => {
     setTasks(...tasks, 
@@ -20,14 +24,19 @@ export default function App() {
     <View style={styles.container}>
       {/* The first one houses the input and button to create a new task */}
       <View style = {styles.inputContainer}> 
-        <TextInput placeholder = 'List Item' style = {styles.input} onChangeText = {onHandlerChangeItem}/>
+        <TextInput placeholder = 'List Item' style = {styles.input} onChangeText = {() => onHandlerChangeItem('Water')}/>
         <Button title = 'Add Task' onPress = {addTask}/>
       </View>
       {/* The second one houses the list with the tasks. */}
       {/* Each task is clickable, and upon beingclicked will open a modal that will allow the deletion of said task */}
       <View style = {styles.itemsListContainer}>
+        {console.log(tasks)}
         {tasks.map( (element) => {
-          <Text id = {element.id}> {element.value} </Text>
+          return (
+            <View style = {styles.itemView}>
+              <Text id = {element.id} style = {{textAlign: 'center'}}> {element.value} </Text>
+            </View>
+          )
         })}
       </View>
       <StatusBar style="auto" />
@@ -39,21 +48,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff456',
+    backgroundColor: '#ffffff',
     padding: 30
   },
   input: {
     borderBottomColor: 'black', 
     borderBottomWidth: 1,
-    innerWidth: 200
+    width: 200,
+    marginRight: 30
   },
   inputContainer: {
+    marginTop: 50,
     flexDirection: 'row',
     justifyContent: 'center',
-    padding: 15,
-    backgroundColor: '#CC5500'
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    backgroundColor: '#DCDCDC',
+    borderRadius: 25,
   },
   itemsListContainer: {
-
+    backgroundColor: '#DDDDDD',
+    marginTop: 20,
+    width: 330,
+    borderRadius: 25,
+    padding: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  itemView: {
+    backgroundColor: '#BBF6F9',
+    height: 45,
+    borderRadius: 25,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
   }
 });
