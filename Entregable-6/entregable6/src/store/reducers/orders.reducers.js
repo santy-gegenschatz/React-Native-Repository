2b5e7b631d7b8237fb28e0267191cf229d646d1f
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-10-15 13:27:17 -0300

    [feature] - improved the orders reducer file

--- Entregable-6/entregable6/src/store/reducers/orders.reducers.js
+++ Entregable-6/entregable6/src/store/reducers/orders.reducers.js
@@ -1,3 +1,16 @@
 const initialState = {
-    
+    orders: []
 }
+
+const ordersReducer = (state = initialState, action) {
+    switch(action.type) {
+        case (GET_ORDERS):
+            return 'a'
+        case (DELETE_ORDERS):
+            return 'b'
+        default:
+            return 'c'
+    }
+}
+
+export default ordersReducer
