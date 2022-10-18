8480f28c2beb23d61267660672fdc9302612df58
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-10-17 22:50:43 -0300

    [minor changes] - changes to the UI

--- Entregable-6/entregable6/src/screens/auth/index.js
+++ Entregable-6/entregable6/src/screens/auth/index.js
@@ -1,12 +1,55 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Button, TextInput, KeyboardAvoidingView} from 'react-native'
+import { colors } from '../../constants/colors.js'
 import { styles } from './styles.js'
 
-const Auth = () => {
+const Auth = ({navigation}) => {
+    const title = 'Register'
+    const message = 'Already have an account ?'
+    const messageAction = 'Register'
+    const messageTarget = 'Log in'
     return (
-        <View style = {styles.container} >
-                <Text style = {styles.title} > This is the login screen</Text>
-        </View>
+        <KeyboardAvoidingView style = {styles.containerKeyboard} behavior = 'padding'>
+            <View style = {styles.container} >
+                    <Text style = {styles.title} > This is the login screen</Text>
+
+                    <Text style = {styles.label} > Email </Text>
+                    <TextInput 
+                        style = {styles.input}
+                        placeholder = 'Enter your email'    
+                        placeholderTextColor = {colors.grey} 
+                        keyboardType = 'email-address'
+                        autoCapitalize = 'none'
+                        autoCorrect = {false}
+                        onChange = {(text) => console.log(text)}
+                    />
+
+                    <Text style = {styles.password} > Password </Text>
+                    <TextInput 
+                        style = {styles.input}
+                        placeholder = 'Enter your password'
+                        placeholderTextColor = {colors.grey} 
+                        secureTextEntry = {true}
+                        autoCapitalize = 'none'
+                        autoCorrect = {false}
+                        onChange = {(text) => console.log(text)}
+                    />
+
+                    <Button 
+                        title = {messageAction}
+                        color = {colors.green}
+                        onPress = {() => console.log('press')}
+                        />
+                    
+                    <View style = {styles.prompt}>
+                        <Text style = {styles.promptMessage}> {message} </Text>
+                        <TouchableOpacity
+                            onPress = {() => console.log('change')}>
+                                <Text style = {styles.promptAction}> {messageTarget} </Text>
+                        </TouchableOpacity>
+                    </View>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
 
--- Entregable-6/entregable6/src/screens/auth/styles.js
+++ Entregable-6/entregable6/src/screens/auth/styles.js
@@ -2,14 +2,28 @@ import { StyleSheet } from "react-native";
 import { colors } from "../../constants/colors";
 
 export const styles = StyleSheet.create({
-    container: {
+    containerKeyboard: {
         flex: 1,
         backgroundColor: colors.beige,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        width: '90%',
+        maxWidth: 400,
+        backgroundColor: colors.beige,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 4,
+        borderColor: colors.black,
+        borderRadius: 25,
+        borderStyle: '',
+        padding: 20
     },
     title: {
         fontSize: 20,
         fontFamily: 'Pacifico-Regular',
         textAlign: 'center'
-    }
+    },
+
 })
