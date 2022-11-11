e15a4473d8b813cf1887898c21cdb065bc928736
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-11-11 15:39:34 -0300

    [bug fix] - removed data

--- Entregable-9/entregable9/src/constants/firebase/index.js
+++ Entregable-9/entregable9/src/constants/firebase/index.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'AIzaSyDdAXXlVJIARhN42wJmRXOpbtT_szFSYh4'
-export const URL_API = 'https://the-spring-store-default-rtdb.firebaseio.com/'
+export const API_KEY = 'AIzaSyA-2IejmE-QBuWRSQJjpOcbrs59dk1StUE'
+export const URL_API = 'https://the-spring-store-4b9c6-default-rtdb.firebaseio.com/'
 export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
 export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`
