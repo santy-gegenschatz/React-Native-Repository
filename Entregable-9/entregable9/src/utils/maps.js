00eca893e097e6043d5f519c101da58687da3551
santy-gegenschatz <gegenschatz.santiago@gmail.com>
2022-11-11 15:54:14 -0300

    fixes

--- Entregable-9/entregable9/src/utils/maps.js
+++ Entregable-9/entregable9/src/utils/maps.js
@@ -1,4 +1,4 @@
-export const API_KEY_MAPS = "";
+export const API_KEY_MAPS = ""
 
 export const URL_MAPS = (lat, lng, zoom = 14) => {
     return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap
